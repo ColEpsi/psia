82d8f7e30e65a92cf6fbd9f892f4ac112c108bc5
Uroš Vaupotič <uv0414@student.uni-lj.si>
2017-11-21 11:14:07 +0100

    edit windows
    Popravljena okna za urejanje

--- Vprasanja/CSharp/edit.php
+++ Vprasanja/CSharp/edit.php
@@ -6,8 +6,7 @@ $id = $_SESSION['session_id'];
 
 $content = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM data_csharp WHERE ID='$id'"));
 //echo "<script>console.log(".$id.");</script>";
-echo "<script>console.log(".$id.");</script>";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['submit'])) {
     echo "<script>console.log(".$id.");</script>";
     $myquestion = mysqli_real_escape_string($db, $_POST['question']);
     $myanswer   = mysqli_real_escape_string($db, $_POST['answer']);
@@ -19,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "UPDATE data_csharp SET question='$myquestion', answer='$myanswer', verified='$verified', tag='$mykeywords' WHERE ID='$id'";
     mysqli_query($db, $sql) or die(mysqli_error($db));
     header("location:admin.php");
-    
+
 }
 
 $message     = "";
@@ -35,7 +34,7 @@ $credentials = $_SESSION['credentials'];
 	<link href="https://www.w3schools.com/w3css/3/w3.css" rel="stylesheet">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.13/js/bootstrap-multiselect.js"></script>
@@ -53,7 +52,7 @@ $credentials = $_SESSION['credentials'];
 			<a class="navbar-brand" href="logged_in.php">Začetna stran</a>
               <a class="navbar-brand" href="javascript:history.back()">Nazaj</a>
 				<ul class="nav fixed-top navbar-nav navbar-right">
-					
+
 					<li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b><span class="glyphicon glyphicon-user"></span> <?php echo $credentials; ?></b> <span class="caret"></span></a>
 			<ul id="login-dp" class="dropdown-menu">
@@ -62,8 +61,8 @@ $credentials = $_SESSION['credentials'];
 							<div class="col-md-12">
 											 <button onclick="window.location.href='logged_in.php'" type="submit" name="submit" class="btn btn-danger btn-block">Odjava</button>
 												<br>
-										
-								 
+
+
 							</div>
 					 </div>
 				</li>
@@ -84,31 +83,31 @@ $credentials = $_SESSION['credentials'];
       <?php echo $message; ?>
     </div>
     <br>
-    <form class="form-horizontal" id="form-input" role="form" method="post" enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF'];?>">  
+    <form class="form-horizontal" id="form-input" role="form" method="post" enctype="multipart/form-data" action="">
       <div class="form-group">
    		<div class="col-sm-12">
    			<label for="question">Vprašanje:</label>
   		<input type="text" class="form-control" id="quesion" name="question"  value="<?php echo $content['question']; ?>">
    		</div>
-		
-	</div> 
+
+	</div>
    	<div class="form-group">
    		<div class="col-sm-12">
   		<textarea class="form-control tinymce" rows="5" id="answer" name="answer">
           <?php
-        echo "<p>".$content['answer']."</p>";  
+        echo "<p>".$content['answer']."</p>";
           ?>
         </textarea>
    		</div>
-		
-	</div>                   
+
+	</div>
       <br>
       <div class="form-group" >
   <div class="col-sm-12">
       <label for="question">Ključne besede:</label>
       <input type="text" class="form-control" id="keywords" name="keywords"  value="<?php echo $content['tag']; ?>">
       </div>
-</div> 
+</div>
     <label class="custom-control custom-checkbox">
   <input type="checkbox" name="verified" id="verified" class="custom-control-input">
   <span class="custom-control-indicator"></span>
@@ -116,15 +115,15 @@ $credentials = $_SESSION['credentials'];
 </label>
       <div style="text-align:center; " class="button">
         <button class="btn btn-primary btn-lg" type="submit" name="submit">Posodobi</button>
-        <button type="button" class="btn btn-lg btn-danger" onclick="window.location.href='delete.php'">Izbriši objavo</button> 
+        <button type="button" class="btn btn-lg btn-danger" onclick="window.location.href='delete.php'">Izbriši objavo</button>
       </div>
-    </form>  
+    </form>
 
     </div>
-    
-    </div> 
+
+    </div>
 		</div>
-		
+
 	</div><!-- Content will go here -->
 </body>
 </html>
